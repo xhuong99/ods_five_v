6c91897ef616f5c25ac58b4c2ae6eba42206de05
phuonghnl <phuonghnlse130019@fpt.edu.vn>
2020-06-05 22:41:27 +0700

    commit tablet screen v.1

---
+++ android/app/src/main/kotlin/com/example/ods_tablet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ods_tablet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/login_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:ods_tablet/dashboard_demo.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+
+
+    final logo = Padding(
+      padding: EdgeInsets.all(20),
+        child: Hero(
+        tag: 'hero',
+        child: CircleAvatar(
+          radius: 56.0,
+          child: Image.asset('assets/Free_Sample_By_Wix.png'),
+        )
+      ),
+    );
+
+
+    final inputPassword = Padding(
+      padding: EdgeInsets.only(bottom: 20),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: 'Password',
+          contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(50.0)
+          )
+        ),
+      ),
+    );
+
+
+    final buttonLogin = Padding(
+      padding: EdgeInsets.only(bottom: 5),
+      child: ButtonTheme(
+        height: 56,
+        child: RaisedButton(
+          child: Text('Login', style: TextStyle(color: Colors.white, fontSize: 20)),
+          color: Colors.black87,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50)
+          ),
+          onPressed: () => {
+            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Dashboard()))
+          },
+        ),
+      ),
+    );
+
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: ListView(
+            shrinkWrap: true,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            children: <Widget>[
+              logo,
+              inputPassword,
+              buttonLogin
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
